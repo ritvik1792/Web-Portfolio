fd7b83c1a764e8b3126616ec8dfca3dd87a1edee
ritvik <ritvikbilla@gmail.com>
2023-08-22 16:49:46 +0530

    added interactions

---
+++ data/journey.js
@@ -0,0 +1,55 @@
+export const records=[
+    {
+        type:"education",
+        name:"Education",
+        data:"B.Tech Computer Science and Engineering",
+        rank:"VIT Vellore - 8.80 CGPA",
+        syear: "2020",
+        eyear: "2024",
+        extra:{}
+    },
+    {
+        type:"certificate",
+        name:"Certification",
+        company:"Microsoft",
+        title:"Security Compliance and Identity Fundamentals",
+        year: "2023",
+        extra:{
+            url:"https://drive.google.com/file/d/1W3CBdEQr3XwKyc9IDlKRVY-blCmw-J8l/view?usp=sharing"
+        }
+    },
+    {
+        type:"certificate",
+        name:"Certification",
+        company:"Coding Blocks",
+        title:"C++ DSA Master Course",
+        year: "2021",
+        extra:{
+            url:"https://drive.google.com/file/d/1wB5sDEJAn9OO8gMwb2HXIv0E5jxCkILB/view?usp=sharing"
+        }
+    },
+    {
+        type:"paper",
+        name:"Research Paper",
+        journal:"IEEE",
+        title:"IoT Based Drip Monitoring System",
+        year: "2022",
+        extra:{
+            url:"https://ieeexplore.ieee.org/document/9753052"
+        }
+    },
+    {
+        type:"education",
+        name:"Education",
+        data:"CBSE 12th Board",
+        rank:"92.2%",
+        syear:"2019",
+        eyear:"2020",
+        extra:{
+            data:"CBSE 10th Board",
+            rank:"94.6%",
+            syear: "2017",
+            eyear: "2018",
+        }
+    },
+]
--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-import {skills} from './data/skills.js'
-import {projects} from './data/projects.js'
+import { skills } from './data/skills.js'
+import { projects } from './data/projects.js'
+import { records } from './data/journey.js'
+
+let _stats_card = document.querySelector('.section_rating');
+async function addCard(_card) {
+  let leetcode = await fetch('https://leetcard.jacoblin.cool/ritvikbilla?animation=false&theme=light&border=0');
+  _card.innerHTML = `
+  <img src="${leetcode.url}" class="leetcode-card" onclick="location.href='https://leetcode.com/ritvikbilla/'"></img>`;
+}
+
+addCard(_stats_card);
+
+let _records = document.querySelector('[data-class="journey"]');
+let _r_text = "";
+records.forEach(_record => {
+  if (_record.type === "education") {
+    _r_text += `
+      <div class="details-container record">
+        <img src="./images/${_record.type}.png" alt="${_record.name} icon" class="icon" />
+        <h3>${_record.name}</h3>
+        <p>${_record.data}<br />${_record.rank}</p>
+        <p style="font-weight:bold; padding: 5px">${_record.syear}-${_record.eyear}</p>
+    `;
+    if (Object.keys(_record.extra).length) {
+      _r_text += `<p>${_record.extra.data}<br />${_record.extra.rank}</p>
+      <p style="font-weight:bold">${_record.extra.syear}-${_record.extra.eyear}</p>`;
+    }
+    _r_text += `</div>`
+  }
+  else if (_record.type === "certificate") {
+    _r_text += `
+      <div class="details-container record" onclick="location.href='${_record.extra.url}'">
+        <img src="./images/${_record.type}.png" alt="${_record.name} icon" class="icon""/>
+        <h3>${_record.name}</h3>
+        <p>${_record.company}<br />${_record.title}</p>
+        <p style="font-weight:bold">${_record.year}</p>
+      </div>
+      `
+  }
+  else if (_record.type === "paper") {
+    _r_text += `
+      <div class="details-container record" onclick="location.href='${_record.extra.url}'">
+        <img src="./images/${_record.type}.png" alt="${_record.type} icon" class="icon" style="1.5rem; height: 1.5rem;"/>
+        <h3>${_record.name}</h3>
+        <p>${_record.journal}<br />${_record.title}</p>
+        <p style="font-weight:bold">${_record.year}</p>
+      </div>
+    `;
+  }
+})
+
+_records.innerHTML = _r_text;
+
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
@@ -8,15 +60,15 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-let _tech_skills=document.querySelector('.skills-container');
-let _s_text="";
-skills.forEach(_type=>{
-  _s_text+=`
+let _tech_skills = document.querySelector('.skills-container');
+let _s_text = "";
+skills.forEach(_type => {
+  _s_text += `
   <div class="details-container">
   <h2 class="experience-sub-title">${_type.type}</h2>
   <div class="article-container">
-      ${_type.data.map(ele=>{
-        return `  
+      ${_type.data.map(ele => {
+    return `  
           <article>
           <img
             src="./images/checkmark.png"
@@ -28,82 +80,71 @@ skills.forEach(_type=>{
             <p>${ele.level}</p>
           </div>
         </article>`
-      }).join("")}
+  }).join("")}
+  </div>
+  </div>
+  `;
+})
+
+_tech_skills.innerHTML = _s_text;
+
+let _projects = document.querySelector('.projects-container');
+let _p_text = "";
+
+projects.forEach(_prj => {
+  _p_text += `
+  <div class="details-container color-container project">
+  <div class="article-container">
+  <img
+  src="./images/${_prj.id}.png"
+  alt="${_prj.name}"
+  class="project-img"
+  />
+  </div>
+  <h2 class="experience-sub-title project-title">${_prj.name}</h2>
+  <h4>${_prj.tools.map(_ele => {
+    return `${_ele}`;
+  }).join(", ")}</h4>
+  <p>${_prj.desc} </p>
+  <div class="btn-container">
+  <button
+  class="btn btn-color-2 project-btn"
+  onclick="location.href='${_prj.github}'"
+  >
+  Github
+  </button>
   </div>
   </div>
   `;
 })
 
 
-let _projects=document.querySelector('.projects-container');
-let _skill_content=document.getElementById('skills');
-let _project_content=document.getElementById('projects');
-let _js_btn_skills=document.querySelector('.js-btn-skills');
-let _js_btn_projects=document.querySelector('.js-btn-projects');
-let _p_text="";
-
-projects.forEach(_prj=>{
-  _p_text+=`
-        <div class="details-container color-container">
-        <div class="article-container">
-          <img
-          src="./images/${_prj.id}.png"
-          alt="${_prj.name}"
-          class="project-img"
-          />
-        </div>
-        <h2 class="experience-sub-title project-title">${_prj.name}</h2>
-        <h4>${_prj.tools.map(_ele=>{
-          return `${_ele}`;
-        }).join(", ")}</h4>
-        <p>${_prj.desc} </p>
-        <div class="btn-container">
-          <button
-          class="btn btn-color-2 project-btn"
-          onclick="location.href='${_prj.github}'"
-          >
-          Github
-        </button>
-        </div>
-      </div>
-  `;
-})
-
-
-_projects.innerHTML=_p_text;
-_tech_skills.innerHTML=_s_text;
-
-// _skill_content.innerHTML=`
-//   <p class="section__text__p1">Explore My</p>
-//   <h1 class="title">Technical Skills</h1>
-//   <div class="experience-details-container">
-//     <div class="about-containers skills-container">
-//       <!-- script added -->
-//       ${_s_text}
-//     </div>
-//   </div>
-//   `;
-
-  console.log(_skill_content.innerHTML);
+_projects.innerHTML = _p_text;
 
 
 
-_js_btn_skills.addEventListener('click',event=>{
+let _skill_content = document.getElementById('skills');
+let _project_content = document.getElementById('projects');
+let _js_btn_skills = document.querySelector('.js-btn-skills');
+let _js_btn_projects = document.querySelector('.js-btn-projects');
+
+_js_btn_skills.addEventListener('click', event => {
 
   _js_btn_skills.classList.add("btn-color-1");
   _js_btn_projects.classList.remove("btn-color-1");
+  _project_content.classList.add("iactive");
+  _skill_content.classList.remove("iactive");
 
-  _js_btn_skills.data.value="selected";
-  _js_btn_projects.data.value="not selected";
 })
 
-_js_btn_projects.addEventListener('click',event=>{
+_js_btn_projects.addEventListener('click', event => {
 
   _js_btn_projects.classList.add("btn-color-1");
   _js_btn_skills.classList.remove("btn-color-1");
 
-  _js_btn_projects.data.value="selected";
-  _js_btn_skills.data.value="not selected";
+  _skill_content.classList.add("iactive");
+  _project_content.classList.remove("iactive");
 })
+
 
 
